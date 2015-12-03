80b2cd5e4c4ca34ba5915be244aba02c51842223
Fedor Isakov <fisakov@gmail.com>
2015-12-03 12:15:34 +0100

    Planning session implementation. Removed Bridge module

---
+++ reactor/Core/src/jetbrains/mps/logicl/reactor/core/ReactorPlanningSession.kt
@@ -0,0 +1,122 @@
+package jetbrains.mps.logicl.reactor.core
+
+/**
+ * @author Fedor Isakov
+ */
+
+import jetbrains.mps.logic.reactor.constraint.*
+import jetbrains.mps.logic.reactor.program.PlanningSession
+import jetbrains.mps.logic.reactor.rule.InvalidConstraintException
+import jetbrains.mps.logic.reactor.rule.Rule
+import java.util.*
+import java.util.Collections.*
+
+
+class ReactorPlanningSession(val name: String) : PlanningSession() {
+
+    class FactoryImpl : PlanningSession.Factory {
+        override fun create(name: String): PlanningSession = ReactorPlanningSession(name)
+    }
+
+    private val myRules = ArrayList<Rule>()
+
+    private val myRegistry = ConstraintRegistry()
+
+    override fun name(): String = name
+
+    override fun constraintSymbols(): Iterable<ConstraintSymbol> =
+            myRegistry.constraintSymbols()
+
+    override fun constraintArgumentTypes(constraintSymbol: ConstraintSymbol): List<Class<*>> =
+            myRegistry.constraintArgTypes(constraintSymbol)
+
+    override fun predicateSymbols(): Iterable<PredicateSymbol> =
+            myRegistry.predicateSymbols()
+
+    override fun solverClass(predicateSymbol: PredicateSymbol): Class<out Queryable>? =
+            myRegistry.solverClass(predicateSymbol)
+
+    override fun addRule(rule: Rule) {
+        myRegistry.update(rule)
+        myRules.add(rule)
+    }
+
+    override fun addRules(rules: Collection<Rule>) {
+        for (r in rules) {
+            myRegistry.update(r)
+        }
+        myRules.addAll(rules)
+    }
+
+    override fun rules(): Iterable<Rule> = unmodifiableCollection(myRules)
+
+    companion object {
+        val ourFactory = FactoryImpl()
+
+        fun init() {
+            setFactory(ourFactory)
+        }
+
+        fun deinit() {
+            clearFactory(ourFactory)
+        }
+    }
+}
+
+private class ConstraintRegistry {
+
+    private val myConstraintArgTypes = HashMap<ConstraintSymbol, List<Class<*>>>()
+
+    private val myPredicateSolvers = HashMap<PredicateSymbol, Class<out Queryable>>()
+
+    fun update(rule: Rule) {
+        for(item in rule.all()) {
+            checkValid(item)
+            introduce(item)
+        }
+    }
+
+    fun checkValid(item: AndItem) {
+        if (item.symbol().arity() != item.arguments().size) {
+            throw InvalidConstraintException("arity mismatch: ${item.symbol()}")
+        }
+        for (arg in item.arguments()) {
+            if (arg == null) {
+                throw InvalidConstraintException("argument is null: ${item.symbol()}")
+            }
+        }
+        when(item) {
+            is Constraint -> {
+                if (myConstraintArgTypes.containsKey(item.symbol())) {
+                    if (!item.argumentTypes().equals(myConstraintArgTypes.get(item.symbol()))) {
+                        throw InvalidConstraintException("argument types mismatch: ${item.symbol()}")
+                    }
+                }
+            }
+            is Predicate  ->  {
+                // nothing
+            }
+            else          -> throw InvalidConstraintException("unknown item: ${item}")
+        }
+    }
+
+    fun introduce(item: AndItem) {
+        when(item) {
+            is Constraint -> myConstraintArgTypes.put(item.symbol(), item.argumentTypes())
+            is Predicate  -> myPredicateSolvers.put(item.symbol(), item.solverClass())
+            else          -> throw InvalidConstraintException("unknown item: ${item}")
+        }
+    }
+
+    fun constraintSymbols(): Iterable<ConstraintSymbol> =
+            unmodifiableSet(myConstraintArgTypes.keys)
+
+    fun constraintArgTypes(symbol: ConstraintSymbol): List<Class<*>> =
+            unmodifiableList(myConstraintArgTypes.getOrDefault(symbol, emptyList()))
+
+    fun predicateSymbols(): Iterable<PredicateSymbol> =
+            unmodifiableSet(myPredicateSolvers.keys)
+
+    fun solverClass(symbol: PredicateSymbol): Class<out Queryable> =
+            myPredicateSolvers.getOrDefault(symbol, throw NoSuchElementException())
+}
---
+++ reactor/Test/test/TestPlanningSession.kt
@@ -0,0 +1,87 @@
+import jetbrains.mps.logic.reactor.program.PlanningSession
+import jetbrains.mps.logic.reactor.rule.InvalidConstraintException
+import jetbrains.mps.logic.reactor.rule.InvalidRuleException
+import jetbrains.mps.logicl.reactor.core.ReactorPlanningSession
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+import java.util.*
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+import kotlin.test.expect
+
+/**
+ * @author Fedor Isakov
+ */
+
+class Simple {
+
+    companion object {
+        @BeforeClass @JvmStatic fun setup() {
+            ReactorPlanningSession.init();
+        }
+    }
+
+    @Before fun beforeTest() {
+        session = PlanningSession.newSession("test")
+    }
+
+    lateinit var session:PlanningSession
+
+
+    @Test fun empty() {
+        session.addRules(ArrayList())
+    }
+
+    @Test(expected = InvalidRuleException::class)
+    fun emptyBody() {
+        session.addRules(arrayListOf(
+            rule("foo",
+                headKept(
+                    constraint("bar")
+                ))
+        ))
+        assertEquals(session.rules().count(), 1)
+    }
+
+    @Test
+    fun replace() {
+        session.addRules(arrayListOf(
+            rule("foo",
+                headReplaced(
+                    constraint("bar")
+                ),
+                body(
+                    constraint("baz")
+                )),
+            rule("qux",
+                headReplaced(
+                    constraint("bar")
+                ),
+                headKept(
+                    constraint("quux")
+                ),
+                body(
+                    constraint("blah")
+                ))
+        ))
+        assertEquals(session.rules().count(), 2)
+    }
+
+    @Test(expected = InvalidConstraintException::class)
+    fun fail() {
+        session.addRules(arrayListOf(
+            rule("foo",
+                headReplaced(
+                    constraint("bar", 1)
+                ),
+                body(
+                    constraint("bar", "1")
+                ))
+        ))
+    }
+}
+
+
+
+
