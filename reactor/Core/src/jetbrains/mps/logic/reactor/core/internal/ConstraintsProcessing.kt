JetBrains/mps-coderules

commit 591865b9cbc2d74d902cca1b2de722285e9b9275

Small opt: skip traversing journal when there're no rules to invalidate/add

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/ConstraintsProcessing.kt
@@ -51,9 +51,7 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
 
     private val execQueue: ExecutionQueue = ExecutionQueue(journalIndex, RuleOrdering(ruleIndex))
 
-    // tags of rule matches discarded at queue execution
-    private val rewrittenRuleMatches: MutableSet<Any> = mutableSetOf<Any>()
-
+    private val invalidatedRulesTags: MutableSet<Any> = mutableSetOf<Any>()
 
     private data class MatchCandidate(val rule: Rule, val occChunk: MatchJournal.OccChunk)
 
@@ -78,9 +76,8 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
      *  - reactivating occurrences that led to invalidated matches
      *  - pruning invalidated occurrences and matches from Dispatcher's state
      */
-    fun invalidateRuleMatches(ruleIds: Set<Any>): Set<Any> {
+    fun invalidateRuleMatches(ruleIds: Set<Any>) {
         val justificationRoots = mutableListOf<Justified>()
-        val allInvalidatedIds = mutableSetOf<Any>()
 
         val it = this.iterator()
         var prevChunk = it.next() // skip initial chunk
@@ -104,7 +101,7 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
 
                 if (chunk is MatchJournal.MatchChunk) {
                     trace.invalidate(chunk.match)
-                    allInvalidatedIds.add(chunk.ruleUniqueTag)
+                    invalidatedRulesTags.add(chunk.ruleUniqueTag)
 
                     // Seems, it's not strictly necessary, because some of its head occurrences are anyway invalidated forever
                     //  and storing this invalid consumed match can make no harm, except some memory overhead.
@@ -130,7 +127,6 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
 
             prevChunk = chunk
         }
-        return allInvalidatedIds
     }
 
     private fun canMatch(rule: Rule, occ: Occurrence): Boolean =
@@ -189,15 +185,15 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
         }
     }
 
-    fun launchQueue(controller: Controller): Pair<FeedbackStatus, Set<Any>> =
-        execQueue.run(controller, this) to rewrittenRuleMatches
+    fun launchQueue(controller: Controller): FeedbackStatus =
+        execQueue.run(controller, this)
 
     /**
      * Clears state unneeded between incremental sessions
      * and returns [SessionToken] with session results.
      */
     fun endSession(): SessionToken {
-        rewrittenRuleMatches.clear()
+        invalidatedRulesTags.clear()
         val histView = view()
         resetStore() // clear observers
         val rules = ArrayList<Rule>().apply { ruleIndex.forEach { add(it) } }
@@ -207,6 +203,8 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
         }
         return SessionTokenImpl(histView, rules, principalState, logicalState.clear())
     }
+
+    fun invalidatedRules(): Set<Any> = invalidatedRulesTags
 
     /**
      * Called to update the state with the currently active constraint occurrence.
@@ -245,7 +243,7 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
             if (activationChunk != null && !isFront()) {
                 val discards = match.matchHeadReplaced().contains(active)
                 if (discards) {
-                    dropDiscardingMatchesFor(activationChunk, rewrittenRuleMatches)
+                    dropDiscardingMatchesFor(activationChunk, invalidatedRulesTags)
                 }
             }
 

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/ControllerImpl.kt
@@ -56,21 +56,24 @@ internal class ControllerImpl (
 
     fun incrLaunch(constraint: Constraint, rulesDiff: RulesDiff): Pair<FeedbackStatus, Set<Any>> {
 
-        val invalidatedRuleTags =
-            profiler.profile<Set<Any>>("invalidation") {
+        if (rulesDiff.removed.isNotEmpty()) {
+            profiler.profile("invalidation") {
                 processing.invalidateRuleMatches(rulesDiff.removed)
             }
-
-        profiler.profile("adding_matches") {
-            processing.addRuleMatches(rulesDiff.added)
         }
 
-        val (status, rewrittenRuleTags) =
-            profiler.profile<Pair<FeedbackStatus, Set<Any>>>("reexecution") {
+        if (rulesDiff.added.count() > 0) {
+            profiler.profile("adding_matches") {
+                processing.addRuleMatches(rulesDiff.added)
+            }
+        }
+
+        val status =
+            profiler.profile<FeedbackStatus>("reexecution") {
                 processing.launchQueue(this)
             }
 
-        return status to invalidatedRuleTags.union(rewrittenRuleTags)
+        return status to processing.invalidatedRules()
     }
 
     fun activate(constraint: Constraint) : FeedbackStatus {

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/ExecutionQueue.kt
@@ -67,7 +67,7 @@ internal class ExecutionQueue(
                 // Handles the case when several matches are added to the same position.
                 //  Then shouldn't replay, because currentPos is valid and more recent (!) than execPos.
                 if (execPos.pos != prevPos) {
-                    processing.replay(processing.logicalState, execPos.pos)
+                    processing.replay(execPos.pos)
                     lastIncrementalRootPos = execPos.pos
                 }
                 prevPos = execPos.pos
@@ -82,7 +82,7 @@ internal class ExecutionQueue(
             } while (execQueue.isNotEmpty())
         }
         // Also replay to the end after queue is fully executed
-        processing.replay(processing.logicalState, processing.last().toPos())
+        processing.replay(processing.last().toPos())
         return status
     }
 

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/MatchJournal.kt
@@ -86,7 +86,7 @@ interface MatchJournal : MutableIterable<MatchJournal.Chunk>, EvidenceSource {
      * Advances journal position to specified position.
      * @throws IllegalStateException when position is not from the future (relative to current pos).
      */
-    fun replay(observable: LogicalStateObservable, futurePos: Pos)
+    fun replay(futurePos: Pos)
 
     /**
      * Walk from specified [from] position in journal until current position
@@ -151,16 +151,14 @@ interface MatchJournal : MutableIterable<MatchJournal.Chunk>, EvidenceSource {
         fun entriesLog(): List<Entry> = entries
 
         data class Entry(val occ: Occurrence, val discarded: Boolean = false) {
-            fun occ(): Occurrence = occ
-            fun discarded(): Boolean = discarded
             override fun toString() = (if (discarded) '-' else '+') + occ.toString()
         }
 
         fun isDescendantOf(chunk: Chunk): Boolean = this.justifiedBy(chunk)
         fun isTopLevel(): Boolean = justifications().size() <= 1 // this condition implies that there're no ancestor chunks
 
-        fun activatedLog(): List<Occurrence> = entriesLog().filter { !it.discarded() }.map { it.occ() }
-        fun discardedLog(): List<Occurrence> = entriesLog().filter { it.discarded() }.map { it.occ() }
+        fun activatedLog(): List<Occurrence> = entriesLog().filter { !it.discarded }.map { it.occ }
+        fun discardedLog(): List<Occurrence> = entriesLog().filter { it.discarded }.map { it.occ }
 
         /**
          * Returns the resulting collection of activated occurrences
@@ -225,8 +223,8 @@ internal class StoreViewImpl(occurrences: Sequence<Occurrence>) : StoreView {
 private fun Iterable<MatchJournal.Chunk.Entry>.allOccurrences(): List<Occurrence> {
     val set = HashSet<Id<Occurrence>>()
     for (it in this) {
-        val idOcc = Id(it.occ() as Occurrence)
-        if (it.discarded()) set.remove(idOcc) else set.add(idOcc)
+        val idOcc = Id(it.occ)
+        if (it.discarded) set.remove(idOcc) else set.add(idOcc)
     }
     return set.map { it.wrapped }
 }

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/MatchJournalImpl.kt
@@ -19,7 +19,6 @@ package jetbrains.mps.logic.reactor.core.internal
 import jetbrains.mps.logic.reactor.core.*
 import jetbrains.mps.logic.reactor.core.internal.MatchJournal.*
 import jetbrains.mps.logic.reactor.evaluation.ConstraintOccurrence
-import jetbrains.mps.logic.reactor.evaluation.MatchJournalView
 import jetbrains.mps.logic.reactor.evaluation.RuleMatch
 import jetbrains.mps.logic.reactor.evaluation.StoreView
 import jetbrains.mps.logic.reactor.logical.Logical
@@ -154,7 +153,7 @@ internal open class MatchJournalImpl(
         replayWith(to, action)
     }
 
-    override fun replay(observable: LogicalStateObservable, futurePos: MatchJournal.Pos) = replayWith(futurePos, {})
+    override fun replay(futurePos: Pos) = replayWith(futurePos, {})
 
     private fun replayWith(futurePos: MatchJournal.Pos, action: (Chunk) -> Unit) {
         do {
@@ -245,7 +244,7 @@ internal open class MatchJournalImpl(
         for (chunk in hist) { // initial chunk is counted too
             chunk.entriesLog().forEach {
                 val idOcc = Id(it.occ)
-                if (it.discarded()) set.remove(idOcc) else set.add(idOcc)
+                if (it.discarded) set.remove(idOcc) else set.add(idOcc)
             }
             if (chunk === current) {
                 return set.map { it.wrapped }.asSequence()

reactor/Test/test/TestIncrementalProgram.kt
@@ -80,7 +80,7 @@ class TestIncrementalProgram {
         (this.journalView as MatchJournal.View).chunks
 
     private fun EvaluationResult.chunksSymbolView() = this.token().chunks().map {
-        it.entriesLog().map { entry -> !entry.discarded() to entry.occ().constraint().symbol() }
+        it.entriesLog().map { entry -> !entry.discarded to entry.occ.constraint().symbol() }
     }
 
     private fun EvaluationResult.lastChunk() = this.token().chunks().last() as MatchJournal.Chunk

reactor/Test/test/TestStoreAwareJournal.kt
@@ -165,7 +165,7 @@ class TestStoreAwareJournal {
                     nchunks shouldBe 4 * 2 // 4 rules, each activates 1 principal occurrence
 
                     // try replay the last chunk
-                    replay(mockController.logicalStateObservable(), lastPos)
+                    replay(lastPos)
 
                     // should change nothing
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("lax"))
@@ -178,11 +178,11 @@ class TestStoreAwareJournal {
                     storeView().allOccurrences().count() shouldBe 0
                     view().chunks.size shouldBe nchunks
 
-                    replay(mockController.logicalStateObservable(), initPos)
+                    replay(initPos)
 
                     storeView().constraintSymbols() shouldBe setOf<ConstraintSymbol>()
 
-                    replay(mockController.logicalStateObservable(), fooPos)
+                    replay(fooPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("foo"))
                     // although storeView() results change, journal remains the same
@@ -249,22 +249,22 @@ class TestStoreAwareJournal {
                     resetStore()
 
 
-                    replay(mockController.logicalStateObservable(), fooPos)
+                    replay(fooPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("foo"))
 
                     // second replay to the same position, nothing should change
-                    replay(mockController.logicalStateObservable(), fooPos)
+                    replay(fooPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("foo"))
 
 
-                    replay(mockController.logicalStateObservable(), quxPos)
+                    replay(quxPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"))
 
                     // second replay to the same position, nothing should change
-                    replay(mockController.logicalStateObservable(), quxPos)
+                    replay(quxPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"))
                 }
@@ -341,27 +341,27 @@ class TestStoreAwareJournal {
                     resetStore()
 
 
-                    replay(mockController.logicalStateObservable(), quxPos)
+                    replay(quxPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("lax"))
 
                     // second replay to the same position inside chunk, nothing should change
-                    replay(mockController.logicalStateObservable(), quxPos)
+                    replay(quxPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("lax"))
 
 
-                    replay(mockController.logicalStateObservable(), shwuxPos)
+                    replay(shwuxPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("lax"), sym0("shwux"))
 
                     // replay inside chunk when non principal-match happenned
-                    replay(mockController.logicalStateObservable(), kexPos)
+                    replay(kexPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("kex"))
 
 
-                    replay(mockController.logicalStateObservable(), lastPos)
+                    replay(lastPos)
 
                     storeView().constraintSymbols() shouldBe setOf(sym0("qux"), sym0("kex"), sym0("buzz"))
                 }
@@ -410,7 +410,7 @@ class TestStoreAwareJournal {
 
                     storeView().allOccurrences().count() shouldBe 0
 
-                    replay(mockController.logicalStateObservable(), savedPos)
+                    replay(savedPos)
 
                     storeView().allOccurrences() shouldBe oldStore
                     currentPos() shouldBe savedPos
@@ -652,7 +652,7 @@ class TestStoreAwareJournal {
                     // store is not longer valid after removing chunks from history, so reset it
                     resetStore()
                     // move to the point where we want to insert new rule
-                    replay(mockController.logicalStateObservable(), continueFrom)
+                    replay(continueFrom)
 
                     // according to the history 'qux' wasn't activated at this point & 'bar1' wasn't discarded
                     storeView().constraintSymbols() shouldBe setOf<ConstraintSymbol>()
@@ -677,7 +677,7 @@ class TestStoreAwareJournal {
                     assertNotEquals(lastPos, currentPos())
 
                     // finally, purely go the the end, applying the rest of the history to the store
-                    replay(mockController.logicalStateObservable(), lastPos)
+                    replay(lastPos)
 
                     currentPos().chunk shouldBeSame lastPos.chunk // we inserted in the middle -- the last chunk should remain the same
                     storeView().constraintSymbols() shouldBe setOf(sym0("bar1"), sym0("qux"), sym0("marker"))

commit af0a118273a3221e2b1d5f3bb581aaa061176571

Fix mistake introduced in 591865b9: return copy of invalidated tags, not ref (fixes MPSCR-50)

reactor/Core/src/jetbrains/mps/logic/reactor/core/internal/ConstraintsProcessing.kt
@@ -204,7 +204,7 @@ internal class ConstraintsProcessing(private var dispatchingFront: Dispatcher.Di
         return SessionTokenImpl(histView, rules, principalState, logicalState.clear())
     }
 
-    fun invalidatedRules(): Set<Any> = invalidatedRulesTags
+    fun invalidatedRules(): Set<Any> = HashSet<Any>(invalidatedRulesTags)
 
     /**
      * Called to update the state with the currently active constraint occurrence.